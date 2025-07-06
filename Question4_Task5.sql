6b16455bc65ea8f88aa5a9d9997933e0e43d49fd
Emmanuel De Vera <124873866+devsdevera@users.noreply.github.com>
2025-07-06 19:07:51 +1200

    Delete Question4_Task5.sql

--- Question4_Task5.sql
+++
@@ -1,6 +0,0 @@
-SELECT a.RobberID, r.Nickname, SUM(a.Share) AS Earnings
-FROM Accomplices AS a
-JOIN Robbers AS r ON r.RobberID = a.RobberID
-GROUP BY a.RobberId, r.Nickname
-HAVING SUM(a.Share) >= 50000
-ORDER BY Earnings DESC;
