3a4490bfacf0b00962e807e9a957dd3434866ed1
Emmanuel De Vera <124873866+devsdevera@users.noreply.github.com>
2025-07-06 19:07:58 +1200

    Delete Question4_Task6.sql

--- Question4_Task6.sql
+++
@@ -1,5 +0,0 @@
-SELECT s.Description, h.RobberID, r.Nickname
-FROM Hasskills AS h
-JOIN Skills AS s ON s.SkillID = h.SkillID
-JOIN Robbers AS r ON r.RobberID = h.RobberID
-ORDER BY s.Description ASC;
