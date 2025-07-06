18c6331d739b1e6bb38552194a7b73254b21cf0f
Emmanuel De Vera <124873866+devsdevera@users.noreply.github.com>
2025-07-06 19:08:17 +1200

    Delete Question5_Task3.sql

--- Question5_Task3.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT p.BankName, p.City
-FROM Plans p
-WHERE NOT EXISTS (
-SELECT * FROM Robberies r
-WHERE r.BankName = p.BankName AND r.City = p.City
-AND EXTRACT(YEAR FROM r.Date) = EXTRACT(YEAR FROM p.PlannedDate)
-);
